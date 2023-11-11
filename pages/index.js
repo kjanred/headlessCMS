578c62b6c9960b46437ff0d3189a337cfcb9f31d
kjanred <janredkarl@gmail.com>
2023-11-10 16:35:51 -0800

    first headless cms

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,31 @@
-import Layout from '../components/layout';
-import Link from 'next/link';
-import { getSortedList } from '../lib/data-firebase';
-
+import Layout from "../components/layout";
+import Link from "next/link";
+import { getSortedList } from "../lib/data";
 
 export async function getStaticProps() {
   const allData = await getSortedList();
   return {
-    props: { allData }
+    props: { allData },
   };
 }
 
 //exports our home page component
-export default function Home( { allData }) {
+export default function Home({ allData }) {
   return (
     <Layout home>
-<h1>List of Dead Names</h1>
-<div className="list-group">
-    {allData && allData.map(
-        ({id, name}) => (
-          <Link key ={id} href={`/people/${id}`} className="list-group-item list-group-item-action">
-            {name}
-          </Link>
-        )
-    )
-    }
-</div>
-</Layout>
+      <h1>List of Dead Names</h1>
+      <div className="list-group">
+        {allData &&
+          allData.map(({ id, name }) => (
+            <Link
+              key={id}
+              href={`/people/${id}`}
+              className="list-group-item list-group-item-action"
+            >
+              {name}
+            </Link>
+          ))}
+      </div>
+    </Layout>
   );
 }
--- pages/people/[id].js
+++ pages/people/[id].js
@@ -1,47 +1,37 @@
 import Layout from "../../components/layout";
-import { getAllIds, getData } from "../../lib/data-firebase";
-
-export async function getStaticProps( { params } ) {
-    const itemData = await getData(params.id);
-    return {
-        props: {
-           itemData 
-        }
-    };
+import { getAllIds, getData } from "../../lib/data";
+export async function getStaticProps({ params }) {
+  const itemData = await getData(params.ID);
+  return {
+    props: {
+      itemData
+    }
+  };
 }
 
 export async function getStaticPaths() {
-    const paths = await getAllIds();
-    return {
-        paths,
-        fallback: false
-    };
+  const paths = await getAllIds();
+  return {
+    paths,
+    fallback: false
+  };
 }
 
-export default function Entry( { itemData } ) {
-    return (
-        <Layout>
-            <article className="card col-6">
-            <div className="card-body">
-            <h3 className="card-title"><strong>Name: </strong>{ itemData.name}</h3>
-            <h4 className="card-subtitle mb-2 text-body-secondary"><strong>Fav color: </strong>{itemData.favColor}</h4>
-            <h4 className="card-subtitle mb-2 text-body-secondary"><strong>Fav food: </strong>{itemData.favFood}</h4>
-            <h4 className="card-subtitle mb-2 text-body-secondary"><strong>Fav animal: </strong>{itemData.favAnimal}</h4>
-            <br />
-            <h4 className="ms-4">skills:</h4>
-            <ul className="ms-4">
-            {itemData.skills && itemData.skills.map(
-        ({id, skill, skillInfo}) => (
-            <li key ={id} className="list-group-item list-group-item-action">
-         <h5>{skill}</h5>
-        <h6>{skillInfo}</h6> 
-            </li>
-        )
-    )
-    }
-    </ul>
-            </div>
-            </article>
-        </Layout>
-    );
+export default function Entry({ itemData }) {
+  return (
+    <Layout>
+      <article className="card col-6">
+        <div className="card-body">
+          <h3 className="card-title">
+            <strong>Name: </strong>
+            {itemData.name}
+          </h3>
+          <h4 className="card-subtitle mb-2 text-body-secondary">
+            <strong>Fav color: </strong>
+            {itemData.post_title}
+          </h4>
+        </div>
+      </article>
+    </Layout>
+  );
 }
