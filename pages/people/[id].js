d4c2a5a296c0cb2d46dae86a56512b5a2f3e80b9
kjanred <janredkarl@gmail.com>
2023-12-01 21:50:27 -0800

    created 3 new rest endpoints and successfully connected the first of which to this app.

--- pages/people/[id].js
+++ pages/people/[id].js
@@ -23,11 +23,11 @@ export default function Entry({ itemData }) {
       <article className="card col-6">
         <div className="card-body">
           <h3 className="card-title">
-            <strong>First Name: </strong> {itemData.acf_fields.first_name} <strong>Last Name: </strong> {itemData.acf_fields.last_name}
+            <strong>First Name: </strong> {itemData.acf_fields.first_name} <strong>Last Name: </strong> {itemData.acf_fields.last_named}
           </h3>
           <h4 className="card-subtitle mb-2 text-body-secondary">
             <strong>Description: </strong>
-            {itemData.acf_fields.description}
+            {itemData.acf_fields.email}
           </h4>
         </div>
       </article>
